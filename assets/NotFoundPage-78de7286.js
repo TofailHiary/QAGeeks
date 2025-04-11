e9087471bf22d481ac7bc1d6e42dc69b24459e10
TofailHiary <tofailhiary@hotmail.com>
2025-04-11 05:50:19 +0300

    Updates

---
+++ assets/NotFoundPage-78de7286.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-fce441a0.js";import{P as r}from"./ProdErrorPage-37687a75.js";function a(){return o.jsx(r,{text:"Page not found.",canRefresh:!1})}export{a as default};
---
+++ assets/SomethingWentWrongPage-cdbe3c1d.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor-fce441a0.js";import{P as t}from"./ProdErrorPage-37687a75.js";function n(){return r.jsx(t,{text:"Something went wrong.",canRefresh:!0})}export{n as default};
