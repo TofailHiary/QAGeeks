a2ee83db6614e1fbbdfbf1daf8461067496515a1
DESKTOP-GPOD68E\Tofail <tofailhiary@hotmail.com>
2025-04-12 20:50:32 +0300

    Updates

---
+++ assets/SomethingWentWrongPage-55468284.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor-dc5a208a.js";import{P as t}from"./ProdErrorPage-92e5d967.js";function n(){return r.jsx(t,{text:"Something went wrong.",canRefresh:!0})}export{n as default};
