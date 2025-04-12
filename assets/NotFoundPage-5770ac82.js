a57250d7b24cf52972ee352395dac4735f3507f3
DESKTOP-GPOD68E\Tofail <tofailhiary@hotmail.com>
2025-04-12 16:35:57 +0300

    Updates

---
+++ assets/NotFoundPage-5770ac82.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-e5427b02.js";import{P as r}from"./ProdErrorPage-430d9cc2.js";function a(){return o.jsx(r,{text:"Page not found.",canRefresh:!1})}export{a as default};
---
+++ assets/SomethingWentWrongPage-879a89b4.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor-e5427b02.js";import{P as t}from"./ProdErrorPage-430d9cc2.js";function n(){return r.jsx(t,{text:"Something went wrong.",canRefresh:!0})}export{n as default};
